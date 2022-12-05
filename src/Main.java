dbea39dd815ae712a71f730262a25e8247d95511
John Snow <shadowstakeus28@gmail.com>
2022-12-05 20:36:28 +0300

    I created few starting classes and added all visual content that will be used in the game

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import arrays.RandomizeAnArray;
-
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        RandomizeAnArray array = new RandomizeAnArray();
 
 
     }
